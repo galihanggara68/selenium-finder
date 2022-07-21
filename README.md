fc305ea10b84c55bedb1b17f699cdd2c00573f5f
galihanggara68 <galihanggara68@gmail.com>
2022-07-21 21:41:59 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,177 @@
+# Selenium Element Finder
+
+## Intro
+
+You can easily find elements (by css selectors) in this case, using just JSON mapping file.
+
+## Usage
+
+### Clone or Install from pip
+
+Clone this repo to your current directory
+
+> git clone https://github.com/galihanggara68/selenium-finder.git
+> or install using pip
+> pip3 install git+https://github.com/galihanggara68/selenium-finder.git@master
+
+### Import library
+
+First of all import Finder class
+`from finder.finder import Finder`
+
+### Using Finder class
+
+`Finder` class has 2 parameters `driver` the Selenium WebDriver and `options`_(optional)_ dictionary where you can customize some options within `Finder` class
+
+```
+from selenium import webdriver
+driver = webdriver.Chrome()
+# global_wait default 10 second
+# iterable_each_wait default 1 second
+options = {"global_wait": 10, "iterable_each_wait": 2}
+finder = Finder(driver, options)
+driver.get("https://example.com")
+# load mapping data
+finder.load_mapping("json_mapping.json")
+# execute finder
+finder.by_json_scheme()
+# get successfully mapped data
+print(finder.get_mapped_data())
+```
+
+## Finder methods
+
+### by_json_scheme()
+
+execute Finder object
+
+### load_mapping(json_path)
+
+load JSON mapping file
+_json_path_ JSON file path
+
+### get_mapping()
+
+get current mapping scheme
+
+### set_mapping(new_mapping)
+
+set mapping scheme as current mapping
+_new_mapping_ mapping dictionary
+
+### get_mapped_data()
+
+get successful mapped data
+
+## JSON mapping
+
+### Basic mapping
+
+```
+{
+    [map_name]: {
+        "type": ["clickable", "typable", "iterable", "text", "value", "attribute"],
+        "elem": [css selector element]
+    }
+}
+```
+
+### text and value mapping
+
+```
+{
+    [map name]: {
+        "type": ["text" | "value"],
+        "elem": [css selector element]
+    }
+}
+```
+
+### attribute mapping
+
+```
+{
+    [map name]: {
+        "type": "attribute",
+        "elem": [css selector element],
+        "attribute": [attribute selector]
+    }
+}
+```
+
+### clickable mapping
+
+`effect` propery will executed after element clicked
+
+```
+{
+    [map name]: {
+        "type": "clickable",
+        "elem": [css selector element],
+        "effect": {
+            [another mapping]
+        }
+    }
+}
+```
+
+### typable mapping
+
+`text` propery is a keys that sent to element
+`enter` boolean, if true it will send enter key
+
+```
+{
+	"search": {
+		"type": "typable",
+		"elem": ".search-global-typeahead__input",
+		"text": "query text",
+		"enter": true
+	},
+}
+```
+
+### iterable mapping
+
+`map` will be a mapper for each item iterated
+
+```
+{
+    [map name]: {
+        "type": "iterable",
+        "elem": [css selector element],
+        "map": {
+            [map name 1]: [selector 1],
+            [map name n]: [selector n],
+        }
+    }
+}
+```
+
+## Mapping Example
+
+```
+{
+	"search": {
+		"type": "typable",
+		"elem": ".search",
+		"text": "Query",
+		"enter": true
+	},
+	"user": {
+		"type": "clickable",
+		"elem": "a.app-aware-link.artdeco-button[href*=Profile]",
+		"effect": {
+			"type": "text",
+			"elem": "section h1"
+		}
+	},
+	"skills": {
+		"type": "iterable",
+		"elem": "#skills ~ div ul > li > div",
+		"map": {
+			"name": "div > div span.t-bold"
+		}
+	}
+}
+```
---
+++ finder.py
@@ -0,0 +1,72 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import json
+
+class Finder:
+    
+    def __init__(self, driver, options = None):
+        self.driver = driver
+        self.global_wait = options["global_wait"] if "global_wait" in options.keys() else 10
+        self.iterable_each_wait = options["iterable_each_wait"] if "iterable_each_wait" in options.keys() else 1
+        self.wait = WebDriverWait(driver, self.global_wait)
+        self.data_map = {};
+        
+    def iterable_callback(self, selector, mapping, parent_key):
+        elems_arr = []
+        for elem in WebDriverWait(self.driver, self.global_wait).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, selector))):
+            elem_obj = {}
+            for key in mapping.keys():
+                try:
+                    elem_obj[key] = WebDriverWait(elem, self.iterable_each_wait).until(EC.visibility_of_element_located((By.CSS_SELECTOR, mapping[key]))).text
+                except TimeoutException:
+                    continue
+            if len(elem_obj.keys()) > 0:
+                elems_arr.append(elem_obj)
+        self.data_map[parent_key] = elems_arr
+                
+    def find_single_map(self, json_map, key):
+        if json_map["type"] == "clickable":
+            elem = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, json_map["elem"])))
+            elem.click()
+            if "effect" in json_map.keys():
+                self.find_single_map(json_map["effect"], key)
+        if json_map["type"] == "iterable":
+            self.iterable_callback(json_map["elem"], json_map["map"], key)
+        if json_map["type"] == "typable":
+            el = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, json_map["elem"])))
+            el.send_keys(json_map["text"])
+            if "enter" in json_map.keys():
+                el.send_keys(Keys.RETURN)
+        if json_map["type"] == "text":
+            self.data_map[key] = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, json_map["elem"]))).text
+        if json_map["type"] == "value":
+            self.data_map[key] = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, json_map["elem"]))).value
+        if json_map["type"] == "attribute":
+            self.data_map[key] = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, json_map["elem"]))).get_attribute(json_map["attribute"])
+    
+    def find_with_maping(self, mapping):
+        for key in mapping.keys():
+            self.find_single_map(mapping[key], key)
+            if "with_refresh" in mapping[key].keys():
+                self.driver.refresh()
+                
+    def load_mapping(self, json_path):
+        with open(json_path, 'r') as json_mapping:
+            self.mapping = json.load(json_mapping)
+            print(self.mapping)
+    
+    def get_mapping(self):
+        return self.mapping
+        
+    def set_mapping(self, new_mapping):
+        self.mapping = new_mapping
+        
+    def by_json_scheme(self):
+        self.find_with_maping(self.mapping)
+        
+    def get_mapped_data(self):
+        return self.data_map
+        
