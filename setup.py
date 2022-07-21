392118faa4536332b07ea99c44487cfa0503210d
galihanggara68 <galihanggara68@gmail.com>
2022-07-21 21:48:33 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Selenium Finder',
+      version='1.0',
+      description='Selenium Finder using JSON mapping file',
+      author='Galih Anggara',
+      author_email='galihanggara68@gmail.com',
+      url='https://github.com/galihanggara68/selenium-finder',
+      packages=['distutils', 'distutils.command'],
+     )
